a6bc71f75df14a7dc66e2153ebe9590bd1a38773
Khalilur Rahman Ridoy Khan <114473324+khalilurrrahmanridoykhan@users.noreply.github.com>
2024-02-05 01:47:34 +0600

    First commit

---
+++ ex01.py
@@ -0,0 +1,14 @@
+x = int(input("Enter First Integer Number: "))
+y = int(input("Enter Secoend Integer Number: "))
+
+
+product = x * y 
+print("The Product of this 2 variable: ",product)
+
+
+if product > 1000:
+    sum1 = x + y
+    print("The Sum of this 2 variable: ",sum1)
+
+
+
